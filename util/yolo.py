3a58acfed7fb4a23feddd572077f4a72e00b5bc8
Stephen Harris <contact@stephenharris.info>
2020-02-01 15:15:13 +0000

    rename as non-maximal suppression

--- util/yolo.py
+++ util/yolo.py
@@ -3,23 +3,65 @@ from collections import namedtuple
 
 Prediction = namedtuple('Prediction', 'class_, confidence, leftx, topy, width height')
 
-def area_of_intersection(a, b):  # returns 0 if predictions don't intersect
+def intersection_over_union(a, b): 
     dx = max(0,min(a.leftx + a.width, b.leftx + b.width) - max(a.leftx, b.leftx))
     dy = max(0, min(a.topy + a.height, b.topy + b.height) - max(a.topy, b.topy))
-    return (dx*dy)/((a.width*a.height) + (b.width*b.height) - (dx*dy));
+    return (dx*dy)/((a.width*a.height) + (b.width*b.height) - (dx*dy)); # IoU (Jaccard_index)
 
-def remove_overlapping_predictions(predictions, threshold=.15):
-    i = 0;
-    while(i + 1 < len(predictions) and i < 10):
-        j = i + 1
 
-        while(j < len(predictions) and j < 10):
-            if(area_of_intersection(predictions[i], predictions[j]) > threshold):
-                removeIndex = i if predictions[i].confidence < predictions[j].confidence else j
-                predictions.remove(predictions[removeIndex])
-            else:
-                j = j +1
-        
-        i = i + 1
+def overlap_coefficient(a,b):
+    dx = max(0,min(a.leftx + a.width, b.leftx + b.width) - max(a.leftx, b.leftx))
+    dy = max(0, min(a.topy + a.height, b.topy + b.height) - max(a.topy, b.topy))
+    return (dx*dy)/(min(a.width*a.height, b.width*b.height)); # Szymkiewicz–Simpson coefficient
 
-    return predictions
+
+def projected_overlap_coefficient(a,b):
+    dx = max(0,min(a.leftx + a.width, b.leftx + b.width) - max(a.leftx, b.leftx))
+    score = (dx)/(min(a.width,b.width)) # Szymkiewicz–Simpson coefficient of projection onto x-plane
+    return score
+
+
+def remove_low_confidence_predictions(predictions, threshold = .25):
+    j = 0
+    filtered = predictions.copy()
+    while(j < len(predictions)):
+        if(predictions[j].confidence < threshold):
+            filtered.remove(predictions[j])
+        j = j +1
+
+    return filtered
+
+
+
+def non_maximal_supression(predictions, intersection_score_fn, threshold=.15):
+
+    if (len(predictions) < 1):
+        return predictions
+
+    filtered = list()
+    original = predictions.copy()
+
+    # Order by most confident first
+    original.sort(key=lambda prediction: prediction.confidence, reverse=False)
+
+    # Add most confident prediction to filtered list
+    a = original.pop()
+    filtered.append(a)
+
+    while(len(original) > 0):
+
+        a = original.pop()
+
+        j = 0
+        score = 0
+        while(j < len(filtered) and score <= threshold):
+            score = intersection_score_fn(a, filtered[j])
+            if(score > threshold):
+                break
+
+            j = j + 1
+
+        if(score <= threshold):
+            filtered.append(a)
+            
+    return filtered
