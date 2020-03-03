ab9a012782eedfffda5b6efe289d9bcbf4fa23fd
Stephen Harris <contact@stephenharris.info>
2020-03-03 23:46:17 +0000

    fix cors issue

--- terraform/inference.tf
+++ terraform/inference.tf
@@ -42,7 +42,6 @@ resource "aws_lambda_function" "inference_lambda" {
   runtime = "python3.7"
   timeout=300
   memory_size=3008
-
 }
 
 data "archive_file" "lambda_zip" {
@@ -100,4 +99,11 @@ resource "aws_s3_bucket" "inference_source" {
   versioning {
     enabled = false
   }
+}
+
+module "inference_gateway_api_cors" {
+  source = "squidfunk/api-gateway-enable-cors/aws"
+  version = "0.3.1"
+  api_id          = aws_api_gateway_rest_api.api.id
+  api_resource_id = aws_api_gateway_resource.resource.id
 }
