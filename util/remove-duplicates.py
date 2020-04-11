03e2b1f0e7346ca2a008a47cab9546b0da42ad00
Stephen Harris <contact@stephenharris.info>
2020-04-11 17:32:06 +0100

    Update remove duplicates utility function to use imagehash

--- util/remove-duplicates.py
+++ util/remove-duplicates.py
@@ -1,23 +1,33 @@
 import glob
 import os
 import hashlib
+import sys
+from PIL import Image
+import imagehash
 
 def remove_duplicates(folder):
     
     img_files = glob.glob( folder + "/*.jpg")
 
     image_dict = dict()
+    image_file_dict = dict()
 
     for filename in img_files:
         with open(filename, 'rb') as image_file:
-            image_hash = hashlib.md5(image_file.read()).hexdigest()
+            image_hash = imagehash.average_hash(Image.open(filename))
+            image_file_hash = hashlib.md5(image_file.read()).hexdigest()
 
-        if image_hash in image_dict:
-            print("File {0} is a duplicate of {1}".format(filename, image_dict[image_hash]))
+
+        if image_file_hash in image_file_dict:
+            print("File {0} is a duplicate of {1}".format(filename, image_file_dict[image_file_hash]))
+        elif image_hash in image_dict:
+            print("File {0} is a POSSIBLE duplicate of {1}".format(filename, image_dict[image_hash]))
+            image_file_dict[image_file_hash] = filename
         else:
             image_dict[image_hash] = filename
+            image_file_dict[image_file_hash] = filename
         
-    print("done {0} images".format(len(img_files)))1
+    print("done {0} images".format(len(img_files)))
 
 
 if __name__ == '__main__':
