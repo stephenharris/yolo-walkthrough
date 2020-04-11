f774e43e184df420c7d70aa4d4ef6cfe1df32baa
Stephen Harris <contact@stephenharris.info>
2020-04-11 22:17:59 +0100

    Remove obsolete scripts

--- save_weights.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "Uploading final weights to s3://${S3_BUCKET_NAME}/weights"
-
-aws s3 sync . s3://${S3_BUCKET_NAME}/weights
--- train.sh
+++
@@ -1,9 +0,0 @@
-set -e
-
-echo "Start training at $(date +"%D %T")"
-nohup /home/ubuntu/darknet/darknet detector train ${DATA_CFG} ${MODEL_CFG} ${PRETRAINED_WEIGHTS} -dont_show -mjpeg_port 8090 -map > /home/ubuntu/${PWD}/${S3_BUCKET_NAME}/yolo.log &
-
-# to continue, for example:
-#nohup /home/ubuntu/darknet/darknet detector train ${DATA_CFG} ${MODEL_CFG} /home/ubuntu/backup/counters-yolov3-tiny_12000.weights -dont_show -mjpeg_port 8090 -map > /home/ubuntu/yolo.log &
-
-echo "Finished training at $(date +"%D %T")"
